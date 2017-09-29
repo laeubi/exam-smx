df3875067f689bb013a406c7f6ab700a80ae115f
laeubi <laeubi@laeubi-soft.de>
2017-09-29 15:51:32 +0200

    Add a ContextBased TestSupport

---
+++ itest/src/test/java/examsmx/camel/ContextBasedCamelTestSupport.java
@@ -0,0 +1,145 @@
+package examsmx.camel;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.NoSuchEndpointException;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.spi.Language;
+import org.apache.camel.test.junit4.CamelTestSupport;
+
+/**
+ * Enables TestSupport for a given CamelContext obtained from a different source
+ * make sure to call {@link #tearDown()} after you are done with this, some
+ * feature will only work by extending this class some will work by making some
+ * protected methods public e.g. the {@link #getMandatoryEndpoint(String)}
+ *
+ */
+public class ContextBasedCamelTestSupport extends CamelTestSupport {
+
+	private CamelContext camelContext;
+
+	public ContextBasedCamelTestSupport(CamelContext camelContext) throws Exception {
+		this.camelContext = camelContext;
+		setUp();
+	}
+
+	@Override
+	public Endpoint resolveMandatoryEndpoint(String uri) {
+		return super.resolveMandatoryEndpoint(uri);
+	}
+
+	@Override
+	public <T extends Endpoint> T resolveMandatoryEndpoint(String uri, Class<T> endpointType) {
+		return super.resolveMandatoryEndpoint(uri, endpointType);
+	}
+
+	@Override
+	public MockEndpoint getMockEndpoint(String uri) {
+		return super.getMockEndpoint(uri);
+	}
+
+	@Override
+	public MockEndpoint getMockEndpoint(String uri, boolean create) throws NoSuchEndpointException {
+		return super.getMockEndpoint(uri, create);
+	}
+
+	@Override
+	public void sendBody(String endpointUri, Object body) {
+		super.sendBody(endpointUri, body);
+	}
+
+	@Override
+	public void sendBody(String endpointUri, Object body, Map<String, Object> headers) {
+		super.sendBody(endpointUri, body, headers);
+	}
+
+	@Override
+	public void sendBodies(String endpointUri, Object... bodies) {
+		super.sendBodies(endpointUri, bodies);
+	}
+
+	@Override
+	public Exchange createExchangeWithBody(Object body) {
+		return super.createExchangeWithBody(body);
+	}
+
+	@Override
+	public void assertExpression(Exchange exchange, String languageName, String expressionText, Object expectedValue) {
+		super.assertExpression(exchange, languageName, expressionText, expectedValue);
+	}
+
+	@Override
+	public void assertPredicate(String languageName, String expressionText, Exchange exchange, boolean expected) {
+		super.assertPredicate(languageName, expressionText, exchange, expected);
+	}
+
+	@Override
+	public Language assertResolveLanguage(String languageName) {
+		return super.assertResolveLanguage(languageName);
+	}
+
+	@Override
+	public void assertMockEndpointsSatisfied(long timeout, TimeUnit unit) throws InterruptedException {
+		super.assertMockEndpointsSatisfied(timeout, unit);
+	}
+
+	@Override
+	public void resetMocks() {
+		super.resetMocks();
+	}
+
+	@Override
+	public <T extends Endpoint> T getMandatoryEndpoint(String uri, Class<T> type) {
+		return super.getMandatoryEndpoint(uri, type);
+	}
+
+	@Override
+	public void disableJMX() {
+		super.disableJMX();
+	}
+
+	@Override
+	public void enableJMX() {
+		super.enableJMX();
+	}
+
+	@Override
+	public Exchange createExchangeWithBody(CamelContext camelContext, Object body) {
+		return super.createExchangeWithBody(camelContext, body);
+	}
+
+	@Override
+	public final boolean isCreateCamelContextPerClass() {
+		return true;
+	}
+
+	@Override
+	public final void startCamelContext() throws Exception {
+		// not needed
+	}
+
+	@Override
+	public final void stopCamelContext() throws Exception {
+		// not needed
+	}
+
+	@Override
+	protected final CamelContext createCamelContext() throws Exception {
+		return camelContext;
+	}
+
+	@Override
+	public Endpoint getMandatoryEndpoint(String uri) {
+		return super.getMandatoryEndpoint(uri);
+	}
+
+	@Override
+	public void assertMockEndpointsSatisfied() throws InterruptedException {
+		super.assertMockEndpointsSatisfied();
+	}
+
+}
